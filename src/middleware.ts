a2c8a68532e335d971f1078d6bcb66cd640de261
Syed zain ali shah <zaingit780@gmail.com>
2024-04-27 23:32:39 +0500

    create  auth provider and wrapped layout with auth provider

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,10 @@ export async function middleware(request: NextRequest) {
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
-  return NextResponse.redirect(new URL("/home", request.url));
+  if (!token && url.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+  return NextResponse.next();
 }
 
 export const config = {
