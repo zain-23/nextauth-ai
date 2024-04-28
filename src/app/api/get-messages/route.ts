65d9ae65cad68c1c35b677e53e891c076cc0ac14
Syed zain ali shah <zaingit780@gmail.com>
2024-04-28 22:53:05 +0500

    create get-messages route and send-messages

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,86 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../sign-up/[...nextauth]/options";
+import UserModel from "@/model/User";
+import { connectDB } from "@/lib/dbConnect";
+import { User } from "next-auth";
+import { NextResponse } from "next/server";
+import mongoose from "mongoose";
+
+export const GET = async (request: Request) => {
+  await connectDB();
+
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Not authentication",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+  const user = session?.user as User;
+  const userId = new mongoose.Types.ObjectId(user._id);
+
+  try {
+    const user = await UserModel.aggregate([
+      {
+        $match: {
+          _id: userId,
+        },
+      },
+      {
+        $unwind: "$messages",
+      },
+      {
+        $sort: {
+          "message.createdAt": -1,
+        },
+      },
+      {
+        $group: {
+          _id: "$_id",
+          messages: {
+            $push: "$messages",
+          },
+        },
+      },
+    ]);
+
+    if (!user || user.length === 0) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        messages: user[0].messages,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.log("Failed get messages");
+    return NextResponse.json(
+      {
+        success: false,
+        messages: "Failed get messages",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,67 @@
+import { connectDB } from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { Message } from "@/model/User";
+import { NextResponse } from "next/server";
+
+export const POST = async (request: Request) => {
+  await connectDB();
+
+  try {
+    const { username, content } = await request.json();
+
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          success: false,
+          messages: "User not found",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    if (!user.isAcceptingMessage) {
+      return NextResponse.json(
+        {
+          success: false,
+          messages: "User is accepting messages",
+        },
+        {
+          status: 403,
+        }
+      );
+    }
+
+    const newMessage = {
+      content,
+      createdAt: new Date(),
+    };
+
+    user.messages.push(newMessage as Message);
+    await user.save();
+
+    return NextResponse.json(
+      {
+        success: true,
+        messages: "Message send successfully",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.log("Error adding messages", error);
+    return NextResponse.json(
+      {
+        success: false,
+        messages: "Interval server error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
