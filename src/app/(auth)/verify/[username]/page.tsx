08b7022b22624b39e5f23457cb0e73719ab19a33
Syed zain ali shah <zaingit780@gmail.com>
2024-05-04 01:39:01 +0500

    complete verify code

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -7,25 +7,31 @@ import {
 } from "@/components/ui/input-otp";
 import { ToastAction } from "@/components/ui/toast";
 import { toast } from "@/components/ui/use-toast";
-import { verifySchema } from "@/schemas/verifySchema";
 import { ApiResponse } from "@/types/ApiResponse";
 import axios, { AxiosError } from "axios";
 import { useState } from "react";
-import * as z from "zod";
+import { Loader } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const Page = ({ params }: { params: { username: string } }) => {
   const [verifyCode, setVerifyCode] = useState<string | null>(null);
+  const [isVerifying, setIsVerifying] = useState<boolean>(false);
+  const router = useRouter();
 
   const onSubmit = async () => {
+    setIsVerifying(true);
     try {
       const response = await axios.post<ApiResponse>("/api/verify-code", {
         username: params.username,
         code: verifyCode,
       });
-      console.log("response", response);
+      toast({
+        title: "Verify Successfully",
+        description: response?.data.message,
+      });
+      router.replace("/");
     } catch (error) {
       console.log("Error in verify user");
-
       const axiosError = error as AxiosError<ApiResponse>;
       toast({
         title: "Verify failed",
@@ -33,10 +39,12 @@ const Page = ({ params }: { params: { username: string } }) => {
         description: axiosError.response?.data.message,
         action: <ToastAction altText="Try again">Try again</ToastAction>,
       });
+    } finally {
+      setIsVerifying(false);
     }
   };
   return (
-    <main className="w-full h-screen flex flex-col justify-center items-center">
+    <main className="w-full h-screen flex flex-col justify-center items-center relative">
       <div className="max-w-2xl">
         <h1 className="mb-5 text-4xl text-center">
           Verify your code{" "}
@@ -65,6 +73,11 @@ const Page = ({ params }: { params: { username: string } }) => {
           <InputOTPSlot index={5} className="w-16 h-16 text-2xl" />
         </InputOTPGroup>
       </InputOTP>
+      {isVerifying ? (
+        <div className="w-full h-full absolute top-0 left-0 z-10 bg-background/60 flex justify-center items-center">
+          <Loader className="animate-spin w-20 h-20" />
+        </div>
+      ) : null}
     </main>
   );
 };
